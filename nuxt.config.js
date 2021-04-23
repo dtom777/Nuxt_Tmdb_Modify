77505a6bccd1bf3d58098c779970dd7e717d9b2e
dtom777 <ethan230204@gmail.com>
2021-04-23 22:01:28 +0900

    method名修正&validate

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,16 @@
-const { TMDB_API_KEY, API_URL,FIREBASE_API_KEY,FIREBASE_PROJECT_ID,FIREBASE_AUTH_DOMAIN,STORAGE_BUCKET,MESSAGING_SENDER_ID,FIREBASE_APP_ID } = process.env
+const {
+  TMDB_API_KEY,
+  API_URL,
+  FIREBASE_API_KEY,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_AUTH_DOMAIN,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID
+} = process.env;
 require("dotenv").config();
 
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 // const webpack = require('webpack')
 
 export default {
@@ -16,37 +25,38 @@ export default {
     FIREBASE_APP_ID
   },
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'server',
+  target: "server",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate(title) {
-      return (title ? `${title} | ` : '') + 'Nuxt_TMDB'
+      return (title ? `${title} | ` : "") + "Nuxt_TMDB";
     },
     // titleTemplate: '%s - Nuxt_TMDB',
     // title: 'Nuxt_TMDB',
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
       // { rel: 'stylesheet', href:"https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap"}
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -54,20 +64,20 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
+    "@nuxtjs/vuetify"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    '@nuxtjs/dotenv',
-    ['@nuxtjs/pwa', { icon: false }],
-    'nuxt-webfontloader'
+    "@nuxtjs/axios",
+    "@nuxtjs/dotenv",
+    ["@nuxtjs/pwa", { icon: false }],
+    "nuxt-webfontloader"
   ],
   webfontloader: {
     google: {
-      families: ['Roboto:400']
+      families: ["Roboto:400"]
     }
   },
 
@@ -88,7 +98,7 @@ export default {
           primary: colors.blue.darken2,
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
+          info: colors.teal.lighten1
           // warning: colors.amber.base,
           // error: colors.deepOrange.accent4,
           // success: colors.green.accent3
@@ -104,16 +114,17 @@ export default {
     //     '_': 'lodash'
     //   })
     // ],
-    vendor: ['lodash','vuetify'],
+    vendor: ["lodash", "vuetify"],
     optimization: {
-      removeAvailableModules: true,
-    },
+      removeAvailableModules: true
+    }
   },
   publicRuntimeConfig: {
     apiUrl: API_URL,
-    tmdbApiKey: process.env.NODE_ENV !== 'development' ? TMDB_API_KEY : undefined,
+    tmdbApiKey:
+      process.env.NODE_ENV !== "development" ? TMDB_API_KEY : undefined
   },
   privateRuntimeConfig: {
     tmdbApiKey: TMDB_API_KEY
-  },
-}
+  }
+};
--- pages/movies/_id.vue
+++ pages/movies/_id.vue
@@ -1,42 +1,91 @@
 <template>
   <v-container fluid>
     <v-row>
-      <v-col align-self="center" cols=12 sm=8>
-          <!-- <h1 class="font-weight-bold">{{ movie.title }}</h1> -->
-          <h2 class="font-weight-bold">{{ movie.title }}</h2>
-          <h3 class="subtitle-1 font-weight-light font-italic" v-show="movie.tagline">〜{{ movie.tagline }}〜</h3>
-          <p class="body-2">{{ movie.overview }}</p>
-          <p class="font-weight-light">
-            {{ new Date(movie.release_date).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" })}}(Released)
+      <v-col align-self="center" cols="12" sm="8">
+        <!-- <h1 class="font-weight-bold">{{ movie.title }}</h1> -->
+        <h2 class="font-weight-bold">{{ movie.title }}</h2>
+        <h3
+          class="subtitle-1 font-weight-light font-italic"
+          v-show="movie.tagline"
+        >
+          〜{{ movie.tagline }}〜
+        </h3>
+        <p class="body-2">{{ movie.overview }}</p>
+        <p class="font-weight-light">
+          {{
+            new Date(movie.release_date).toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "long",
+              day: "numeric"
+            })
+          }}(Released)
+        </p>
+        <v-chip-group column>
+          <v-chip
+            outlined
+            label
+            class="font-weight-bold"
+            :small="$vuetify.breakpoint.xsOnly"
+            >Genres</v-chip
+          >
+          <v-chip
+            v-for="genre in genres"
+            :key="genre"
+            class="font-weight-light"
+            :small="$vuetify.breakpoint.xsOnly"
+            >{{ genre }}</v-chip
+          >
+        </v-chip-group>
+        <v-chip-group column>
+          <v-chip
+            outlined
+            label
+            class="font-weight-bold"
+            :small="$vuetify.breakpoint.xsOnly"
+            >Cast</v-chip
+          >
+          <v-chip
+            class="font-weight-light"
+            :small="$vuetify.breakpoint.xsOnly"
+            >{{ credits[0] }}</v-chip
+          >
+          <v-chip
+            class="font-weight-light"
+            :small="$vuetify.breakpoint.xsOnly"
+            >{{ credits[1] }}</v-chip
+          >
+          <v-chip
+            class="font-weight-light"
+            :small="$vuetify.breakpoint.xsOnly"
+            >{{ credits[2] }}</v-chip
+          >
+        </v-chip-group>
+        <div>
+          <v-chip
+            outlined
+            label
+            class="font-weight-bold"
+            :small="$vuetify.breakpoint.xsOnly"
+            >Popularity</v-chip
+          >
+          <v-rating
+            :empty-icon="iconStarOutline"
+            :full-icon="iconStar"
+            :half-icon="iconStarHalf"
+            :value="movie.vote_average / 2"
+            length="5"
+            readonly
+            size="35"
+            color="orange"
+            class="d-inline"
+            :class="{ 'px-0': $vuetify.breakpoint.xsOnly }"
+          ></v-rating>
+          <p class="d-inline">
+            {{ Math.round((movie.vote_average / 2) * 10) / 10 }} / 5
           </p>
-          <v-chip-group column>
-            <v-chip outlined label class="font-weight-bold"  :small='$vuetify.breakpoint.xsOnly'>Genres</v-chip>
-            <v-chip v-for="genre in genres" :key="genre" class="font-weight-light" :small='$vuetify.breakpoint.xsOnly'>{{ genre }}</v-chip>
-          </v-chip-group>
-          <v-chip-group column>
-            <v-chip outlined label class="font-weight-bold" :small='$vuetify.breakpoint.xsOnly'>Cast</v-chip>
-            <v-chip class="font-weight-light" :small='$vuetify.breakpoint.xsOnly'>{{ credits[0] }}</v-chip>
-            <v-chip class="font-weight-light" :small='$vuetify.breakpoint.xsOnly'>{{ credits[1] }}</v-chip>
-            <v-chip class="font-weight-light" :small='$vuetify.breakpoint.xsOnly'>{{ credits[2] }}</v-chip>
-          </v-chip-group>
-          <div>
-            <v-chip outlined label class="font-weight-bold" :small='$vuetify.breakpoint.xsOnly'>Popularity</v-chip>
-            <v-rating
-              :empty-icon="iconStarOutline"
-              :full-icon="iconStar"
-              :half-icon="iconStarHalf"
-              :value="movie.vote_average/2"
-              length="5"
-              readonly
-              size="35"
-              color="orange"
-              class="d-inline"
-              :class='{ "px-0": $vuetify.breakpoint.xsOnly }'
-            ></v-rating>
-            <p class="d-inline">{{ Math.round(movie.vote_average/2 * 10) /10 }} / 5</p>
-          </div>
+        </div>
       </v-col>
-      <v-col cols=12 sm=4>
+      <v-col cols="12" sm="4">
         <v-img
           :src="`https://image.tmdb.org/t/p/w300/${movie.poster_path}`"
           class="rounded"
@@ -95,8 +144,20 @@
             rows="3"
           ></v-textarea>
           <div class="d-flex justify-center">
-            <v-btn @click="add" class="mt-4 py-4 mr-4" color="blue" aria-label="Save">Save</v-btn>
-            <v-btn @click="reset" class="mt-4 py-4" outlined aria-label="Clear">Clear</v-btn>
+            <v-btn
+              @click="addMemo"
+              class="mt-4 py-4 mr-4"
+              color="blue"
+              aria-label="Save"
+              >Save</v-btn
+            >
+            <v-btn
+              @click="resetMemo"
+              class="mt-4 py-4"
+              outlined
+              aria-label="Clear"
+              >Clear</v-btn
+            >
           </div>
         </v-form>
       </v-col>
@@ -105,7 +166,16 @@
 </template>
 
 <script>
-import { mdiCalendar, mdiEmoticonKiss, mdiHandOkay, mdiHandLeft, mdiStarOutline, mdiStar, mdiStarHalfFull} from '@mdi/js'
+import { mapGetters } from "vuex";
+import {
+  mdiCalendar,
+  mdiEmoticonKiss,
+  mdiHandOkay,
+  mdiHandLeft,
+  mdiStarOutline,
+  mdiStar,
+  mdiStarHalfFull
+} from "@mdi/js";
 // import axios from 'axios'
 // import { mapGetters } from 'vuex'
 
@@ -117,8 +187,8 @@ export default {
   },
   head() {
     return {
-      title: 'moives'
-    }
+      title: "moives"
+    };
   },
   data() {
     return {
@@ -132,48 +202,62 @@ export default {
       value: 5,
       menu: false,
       date: null,
-      who: '',
-      items: ['Family', 'Lover', 'Fridends', 'Coworker'],
-      good: '',
-      bad: '',
-      movieId: this.$route.params.id,
-    }
+      who: "",
+      items: ["Family", "Lover", "Fridends", "Coworker"],
+      good: "",
+      bad: "",
+      movieId: this.$route.params.id
+    };
   },
   watch: {
-    menu(val){
-      val && setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'))
+    menu(val) {
+      val && setTimeout(() => (this.$refs.picker.activePicker = "YEAR"));
     }
   },
+  computed: mapGetters(["getLoginUser"]),
   async asyncData({ $axios, $config, params }) {
-    const movie = await $axios.$get(`${$config.apiUrl}/movie/${params.id}?api_key=${$config.tmdbApiKey}`)
-    const credits = await $axios.$get(`${$config.apiUrl}/movie/${params.id}/credits?api_key=${$config.tmdbApiKey}`)
-      return { movie, genres: movie.genres.map(obj => obj.name), credits: credits.cast.map(obj => obj.name)}
+    const movie = await $axios.$get(
+      `${$config.apiUrl}/movie/${params.id}?api_key=${$config.tmdbApiKey}`
+    );
+    const credits = await $axios.$get(
+      `${$config.apiUrl}/movie/${params.id}/credits?api_key=${$config.tmdbApiKey}`
+    );
+    return {
+      movie,
+      genres: movie.genres.map(obj => obj.name),
+      credits: credits.cast.map(obj => obj.name)
+    };
   },
   methods: {
-    preserve(date){
-      this.$refs.menu.save(date)
+    preserve(date) {
+      this.$refs.menu.save(date);
     },
-    add() {
-      let obj = {
-        poster_path: this.movie.poster_path,
-        date: this.date,
-        who: this.who,
-        good: this.good,
-        bad: this.bad,
-        movieId: this.movieId,
-        title: this.movie.title
+    addMemo() {
+      if (this.getLoginUser != null) {
+        let obj = {
+          poster_path: this.movie.poster_path,
+          date: this.date,
+          who: this.who,
+          good: this.good,
+          bad: this.bad,
+          movieId: this.movieId,
+          title: this.movie.title
+        };
+        this.$store.dispatch("add", obj);
+        this.date = "";
+        this.who = "";
+        this.good = "";
+        this.bad = "";
+        alert("Save completed!");
+        this.$router.push("/storage");
+      } else {
+        alert("Please Login!");
+        this.$router.push(`/movies/${this.movieId}`);
       }
-      this.$store.dispatch('add', obj)
-      this.date = ''
-      this.who = ''
-      this.good = ''
-      this.bad = ''
-      alert('Save completed!')
-      this.$router.push('/storage')
     },
-    reset() {
-      this.$refs.form.reset()
-    },
-  },
-}
+    resetMemo() {
+      this.$refs.form.reset();
+    }
+  }
+};
 </script>
